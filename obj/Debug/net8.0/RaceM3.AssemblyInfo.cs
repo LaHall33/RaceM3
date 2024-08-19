6e85f6f1cefdb16ca2cbeed5a5cb74210182f49b
LaHall33 <lanashahall33@gmail.com>
2024-08-19 13:18:09 -0500

    Wireframe added

--- obj/Debug/net8.0/RaceM3.AssemblyInfo.cs
+++ obj/Debug/net8.0/RaceM3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RaceM3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b126ee9651b1f76d689a4533ea407f819380db96")]
 [assembly: System.Reflection.AssemblyProductAttribute("RaceM3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RaceM3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
